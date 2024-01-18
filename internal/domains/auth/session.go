8e3a2fcca1865b0cec307d578da7931083488748
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-01-18 16:20:06 -0300

    refactor: move auth token to types and rename it

--- internal/domains/auth/session.go
+++ internal/domains/auth/session.go
@@ -4,7 +4,6 @@ import (
 	"sort"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/kgjoner/cornucopia/helpers/htypes"
 	"github.com/kgjoner/cornucopia/helpers/normalizederr"
@@ -37,7 +36,7 @@ type SessionCreationFields struct {
 	Ip          string      `json:"ip" validator:"required"`
 }
 
-func (a *Account) InitSession(f *SessionCreationFields) (*Session, *Jwt, *Jwt, error) {
+func (a *Account) InitSession(f *SessionCreationFields) (*authToken, *authToken, error) {
 	// Check if link exists
 	isLinked := false
 	for _, l := range a.Links {
@@ -48,7 +47,7 @@ func (a *Account) InitSession(f *SessionCreationFields) (*Session, *Jwt, *Jwt, e
 	}
 
 	if !isLinked {
-		return nil, nil, nil, normalizederr.NewRequestError("Account is not linked to desired application.", "")
+		return nil, nil, normalizederr.NewRequestError("Account is not linked to desired application.", "")
 	}
 
 	// Terminate exceeding sessions
@@ -62,7 +61,7 @@ func (a *Account) InitSession(f *SessionCreationFields) (*Session, *Jwt, *Jwt, e
 		for _, s := range sessionsToTerminate {
 			_, err := a.TerminateSession(s.Id)
 			if err != nil {
-				return nil, nil, nil, err
+				return nil, nil, err
 			}
 		}
 	}
@@ -80,20 +79,20 @@ func (a *Account) InitSession(f *SessionCreationFields) (*Session, *Jwt, *Jwt, e
 	}
 
 	// Generate tokens
-	refreshToken, err := newJwt(*a, s.Id, "refresh")
+	refreshToken, err := newAuthToken(*a, s.Id, "refresh")
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, err
 	}
 
 	s.updateRefreshToken(*refreshToken)
 
-	accessToken, err := newJwt(*a, s.Id)
+	accessToken, err := newAuthToken(*a, s.Id)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, err
 	}
 
 	a.ActiveSessions = append(a.ActiveSessions, *s)
-	return s, accessToken, refreshToken, validator.Validate(s)
+	return accessToken, refreshToken, validator.Validate(s)
 }
 
 /* ==============================================================================
@@ -109,7 +108,7 @@ func (s *Session) terminate() error {
 	return validator.Validate(s)
 }
 
-func (s *Session) updateRefreshToken(token Jwt) {
+func (s *Session) updateRefreshToken(token authToken) {
 	s.RefreshToken = hashData(token.String())
 	s.UpdatedAt = time.Now()
 }
@@ -135,106 +134,4 @@ func (a SessionSortableByAge) Less(i, j int) bool {
 
 func (a SessionSortableByAge) Swap(i, j int) {
 	a[i], a[j] = a[j], a[i]
-}
-
-/* ==============================================================================
-	JWT
-============================================================================== */
-
-type Jwt struct {
-	jwt.Token
-	Claims       jwtClaims
-	signedString string
-}
-
-func ParseJwtString(str string) (*Jwt, error) {
-	token, err := jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
-		return []byte(config.Environment.JWT.SECRET), nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	if !token.Valid {
-		return nil, normalizederr.NewRequestError("Invalid token", "")
-	}
-
-	claims, ok := token.Claims.(jwtClaims)
-	if !ok {
-		return nil, normalizederr.NewRequestError("Badly formatted token", "")
-	}
-
-	return &Jwt{*token, claims, str}, nil
-}
-
-func newJwt(a Account, sessionId uuid.UUID, kindFlag ...string) (*Jwt, error) {
-	s := a.Session(sessionId)
-	if s == nil {
-		return nil, normalizederr.NewRequestError("Account and session do not match.", "")
-	}
-
-	now := time.Now()
-	var kind string
-	var duration time.Duration
-	if len(kindFlag) >= 1 && kindFlag[0] == "refresh" {
-		kind = "refresh"
-		duration = time.Second * time.Duration(config.Environment.JWT.REFRESH_LIFE_TIME_IN_SEC)
-	} else {
-		kind = "access"
-		duration = time.Second * time.Duration(config.Environment.JWT.ACCESS_LIFE_TIME_IN_SEC)
-	}
-
-	claims := jwtClaims{
-		a.Id,
-		s.Application.Id,
-		now,
-		now.Add(duration),
-		s.Id,
-		kind,
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-	tokenAsSignedString, err := token.SignedString([]byte(config.Environment.JWT.SECRET))
-	if err != nil {
-		return nil, err
-	}
-
-	return &Jwt{*token, claims, tokenAsSignedString}, nil
-}
-
-func (t Jwt) IsExpired() bool {
-	now := time.Now()
-	return t.Claims.Exp.Before(now)
-}
-
-func (t Jwt) String() string {
-	return t.signedString
-}
-
-type jwtClaims struct {
-	Sub       uuid.UUID `json:"sub"`
-	Aud       uuid.UUID `json:"aud"`
-	Iat       time.Time `json:"iat"`
-	Exp       time.Time `json:"exp"`
-	SessionId uuid.UUID `json:"sessionId"`
-	Kind      string    `json:"kind" validate:"oneof=refresh access"`
-}
-
-func (c jwtClaims) GetAudience() (jwt.ClaimStrings, error) {
-	return jwt.ClaimStrings{c.Aud.String()}, nil
-}
-func (c jwtClaims) GetExpirationTime() (*jwt.NumericDate, error) {
-	return &jwt.NumericDate{Time: c.Exp}, nil
-}
-func (c jwtClaims) GetIssuedAt() (*jwt.NumericDate, error) {
-	return &jwt.NumericDate{Time: c.Iat}, nil
-}
-func (c jwtClaims) GetIssuer() (string, error) {
-	return "", nil
-}
-func (c jwtClaims) GetNotBefore() (*jwt.NumericDate, error) {
-	return &jwt.NumericDate{}, nil
-}
-func (c jwtClaims) GetSubject() (string, error) {
-	return c.Sub.String(), nil
 }
--- internal/domains/auth/types.go
+++ internal/domains/auth/types.go
@@ -1,5 +1,14 @@
 package auth
 
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/kgjoner/cornucopia/helpers/normalizederr"
+	"github.com/kgjoner/sphinx/internal/config"
+)
+
 /* ==============================================================================
 	AccountCodeKind
 ============================================================================== */
@@ -41,3 +50,105 @@ func (s Role) Enumerate() any {
 }
 
 var RoleValues = Role.Enumerate("").(role)
+
+/* ==============================================================================
+	Auth Token
+============================================================================== */
+
+type authToken struct {
+	jwt.Token
+	Claims       jwtClaims
+	signedString string
+}
+
+func newAuthToken(a Account, sessionId uuid.UUID, kindFlag ...string) (*authToken, error) {
+	s := a.Session(sessionId)
+	if s == nil {
+		return nil, normalizederr.NewRequestError("Account and session do not match.", "")
+	}
+
+	now := time.Now()
+	var kind string
+	var duration time.Duration
+	if len(kindFlag) >= 1 && kindFlag[0] == "refresh" {
+		kind = "refresh"
+		duration = time.Second * time.Duration(config.Environment.JWT.REFRESH_LIFE_TIME_IN_SEC)
+	} else {
+		kind = "access"
+		duration = time.Second * time.Duration(config.Environment.JWT.ACCESS_LIFE_TIME_IN_SEC)
+	}
+
+	claims := jwtClaims{
+		a.Id,
+		s.Application.Id,
+		now,
+		now.Add(duration),
+		s.Id,
+		kind,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenAsSignedString, err := token.SignedString([]byte(config.Environment.JWT.SECRET))
+	if err != nil {
+		return nil, err
+	}
+
+	return &authToken{*token, claims, tokenAsSignedString}, nil
+}
+
+func ParseAuthToken(str string) (*authToken, error) {
+	token, err := jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.Environment.JWT.SECRET), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, normalizederr.NewRequestError("Invalid authToken", "")
+	}
+
+	claims, ok := token.Claims.(jwtClaims)
+	if !ok {
+		return nil, normalizederr.NewRequestError("Badly formatted authToken", "")
+	}
+
+	return &authToken{*token, claims, str}, nil
+}
+
+func (t authToken) IsExpired() bool {
+	now := time.Now()
+	return t.Claims.Exp.Before(now)
+}
+
+func (t authToken) String() string {
+	return t.signedString
+}
+
+type jwtClaims struct {
+	Sub       uuid.UUID `json:"sub"`
+	Aud       uuid.UUID `json:"aud"`
+	Iat       time.Time `json:"iat"`
+	Exp       time.Time `json:"exp"`
+	SessionId uuid.UUID `json:"sessionId"`
+	Kind      string    `json:"kind" validate:"oneof=refresh access"`
+}
+
+func (c jwtClaims) GetAudience() (jwt.ClaimStrings, error) {
+	return jwt.ClaimStrings{c.Aud.String()}, nil
+}
+func (c jwtClaims) GetExpirationTime() (*jwt.NumericDate, error) {
+	return &jwt.NumericDate{Time: c.Exp}, nil
+}
+func (c jwtClaims) GetIssuedAt() (*jwt.NumericDate, error) {
+	return &jwt.NumericDate{Time: c.Iat}, nil
+}
+func (c jwtClaims) GetIssuer() (string, error) {
+	return "", nil
+}
+func (c jwtClaims) GetNotBefore() (*jwt.NumericDate, error) {
+	return &jwt.NumericDate{}, nil
+}
+func (c jwtClaims) GetSubject() (string, error) {
+	return c.Sub.String(), nil
+}
