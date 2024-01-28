8b6f9230ffbf5e322e57a29bb558cc2d01ebd740
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-01-28 19:40:45 -0300

    feat: raise server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/kgjoner/sphinx/internal/config"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/kgjoner/sphinx/internal/config"
+	"github.com/kgjoner/sphinx/internal/server"
+)
 
 func main() {
 	config.Must()
+	db := setupPostgres()
+
+	server.New(db).Start()
+}
+
+func setupPostgres() *sql.DB {
+	db, err := sql.Open("postgres", config.Environment.DATABASE_URL)
+	if err != nil {
+		log.Fatalf("Unable to parse database url: %v", err)
+	}
+
+	return db
 }
---
+++ internal/server/server.go
@@ -0,0 +1,93 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/kgjoner/sphinx/internal/common"
+	"github.com/kgjoner/sphinx/internal/domains/auth/gateway"
+	authrepo "github.com/kgjoner/sphinx/internal/domains/auth/repository"
+	"github.com/kgjoner/sphinx/postgres"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Server struct {
+	queries *psqlrepo.Queries
+}
+
+func New(pool *sql.DB) *Server {
+	return &Server{
+		queries: psqlrepo.New(pool),
+	}
+}
+
+func (s *Server) Start() {
+	repos := common.Repos{
+		AuthRepo: authrepo.New(s.queries),
+	}
+
+	r := chi.NewRouter()
+	r.Use(addContextToReposAndServices(&repos))
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   allowedOrigins,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	// Api versioning
+	r.Route("/v1", func(r chi.Router) {
+		r.Use(countRequestMetric())
+
+		authgtw.Raise(r, repos)
+	})
+
+	r.Mount("/metrics", promhttp.Handler())
+
+	fmt.Println("Server running at port 8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func addContextToReposAndServices(repos *common.Repos) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+
+			type WithContext interface {
+				AddContext(context.Context)
+			}
+
+			repos := []any{repos.AuthRepo}
+			for _, repo := range repos {
+				if ctxRepo, ok := repo.(WithContext); ok {
+					ctxRepo.AddContext(ctx)
+				}
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+var (
+	RequestCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "api_request_count",
+		Help: "The total number of requests",
+	})
+)
+
+func countRequestMetric() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			RequestCounter.Inc()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
