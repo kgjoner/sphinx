ae68b42411c4d2677fb93db2b05ec8436e0571e8
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-02-04 19:17:10 -0300

    feat(auth): handle duplicate values error

--- internal/domains/auth/cases/account/createAccount.go
+++ internal/domains/auth/cases/account/createAccount.go
@@ -1,6 +1,11 @@
 package accountcase
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/kgjoner/cornucopia/helpers/normalizederr"
 	"github.com/kgjoner/sphinx/internal/domains/auth"
 	authcase "github.com/kgjoner/sphinx/internal/domains/auth/cases"
 )
@@ -19,23 +24,30 @@ func (i CreateAccount) Execute(input CreateAccountInput) (*auth.Account, error) 
 	if err != nil {
 		return nil, err
 	}
-	
+
+	id, err := i.AuthRepo.InsertAccount(*acc)
+	if err != nil {
+		if strings.Contains(err.Error(), "duplicate key") {
+			pattern := regexp.MustCompile("account_(.+)_key")
+			matches := pattern.FindStringSubmatch(err.Error())
+			msg := fmt.Sprintf("%v has already registered", matches[1])
+			return nil, normalizederr.NewRequestError(msg, "")
+		}
+		return nil, err
+	}
+
+	// TODO: Send emails
+
+	acc.InternalId = id
 	err = acc.LinkTo(input.Application)
 	if err != nil {
 		return nil, err
 	}
-	
-	// TODO: Send emails
-	
-	_, err = i.AuthRepo.InsertAccount(*acc)
-	if err != nil {
-		return nil, err
-	}
-	
+
 	err = i.AuthRepo.UpsertLinks(acc.LinksToPersist()...)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return acc, nil
 }
