415b586d5ee3c0facf350b218dae048d22d1b095
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-02-04 19:19:12 -0300

    feat(auth): add login and logout routes

--- internal/domains/auth/gateway/authgtw.go
+++ internal/domains/auth/gateway/authgtw.go
@@ -1,8 +1,13 @@
 package authgtw
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/kgjoner/cornucopia/helpers/controller"
+	"github.com/kgjoner/cornucopia/helpers/presenter"
 	"github.com/kgjoner/sphinx/internal/common"
+	authcase "github.com/kgjoner/sphinx/internal/domains/auth/cases"
 )
 
 type AuthGateway struct {
@@ -18,5 +23,61 @@ func Raise(router chi.Router, repos common.Repos) {
 		},
 	}
 
-	router.Mount("/account", authgtw.accountHandler())
+	router.Route("/account", authgtw.accountHandler)
+
+	router.Route("/auth", func(r chi.Router) {
+		r.With(authgtw.mid.AppToken).Post("/login", authgtw.login)
+		r.With(authgtw.mid.Authenticate).Post("/logout", authgtw.logout)
+	})
+}
+
+func (g AuthGateway) login(w http.ResponseWriter, r *http.Request) {
+	c := controller.New(r).
+		ParseBody("entry", "password").
+		AddApplication().
+		AddHeader("ip").
+		AddHeader("device")
+
+	var input authcase.LoginInput
+	err := c.Write(&input)
+	if err != nil {
+		presenter.HttpError(err, w, r)
+		return
+	}
+
+	i := authcase.Login{
+		AuthRepo: g.AuthRepo,
+	}
+
+	output, err := i.Execute(input)
+	if err != nil {
+		presenter.HttpError(err, w, r)
+		return
+	}
+
+	presenter.HttpSuccess(output, w, r)
+}
+
+func (g AuthGateway) logout(w http.ResponseWriter, r *http.Request) {
+	c := controller.New(r).
+		AddActor()
+
+	var input authcase.LogoutInput
+	err := c.Write(&input)
+	if err != nil {
+		presenter.HttpError(err, w, r)
+		return
+	}
+
+	i := authcase.Logout{
+		AuthRepo: g.AuthRepo,
+	}
+
+	output, err := i.Execute(input)
+	if err != nil {
+		presenter.HttpError(err, w, r)
+		return
+	}
+
+	presenter.HttpSuccess(output, w, r)
 }
