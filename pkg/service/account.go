d71101ad60e8f5af46554e339b3bb4edcdc3e575
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-12-18 11:47:35 -0300

    feat(pkg): add new methods to service

--- pkg/service/account.go
+++ pkg/service/account.go
@@ -3,6 +3,8 @@ package sphinx
 import (
 	"database/sql"
 
+	"github.com/google/uuid"
+	"github.com/kgjoner/cornucopia/helpers/htypes"
 	"github.com/kgjoner/cornucopia/helpers/presenter"
 	"github.com/kgjoner/cornucopia/utils/httputil"
 )
@@ -38,6 +40,60 @@ func (s Service) AccountOf(target string, token string) (*Account, error) {
 	}
 
 	return &respData.Data, nil
+}
+
+// Create a simple account for the informed email.
+func (s Service) NewAccount(email htypes.Email, password string) (accountId uuid.UUID, err error) {
+	body := map[string]any{
+		"email": email,
+		"password": password,
+	}
+
+	var respData presenter.Success[Account]
+	_, err = s.httpApi.Post("/account", body, &httputil.Options{
+		Headers: map[string]string{
+			"X-App": s.appId,
+		},
+	})(&respData)
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return respData.Data.Id, nil
+}
+
+// Check whether entry exists.
+func (s Service) DoesEntryExist(entry string) (bool, error) {
+	var respData presenter.Success[bool]
+	_, err := s.httpApi.Get("/account/existence", &httputil.Options{
+		Headers: map[string]string{
+			"X-Entry": entry,
+		},
+	})(&respData)
+
+	if err != nil {
+		return false, err
+	}
+
+	return respData.Data, nil
+}
+
+// Get account id by their entry. Return nil if entry is not found.
+func (s Service) AccountIdByEntry(entry string) (*uuid.UUID, error) {
+	var respData presenter.Success[*uuid.UUID]
+	_, err := s.httpApi.Get("/account/id", &httputil.Options{
+		Headers: map[string]string{
+			"Authorization": "Basic " + s.appToken,
+			"X-Entry": entry,
+		},
+	})(&respData)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return respData.Data, nil
 }
 
 // Add roles and/or grantings to target account.
