0206aa1f099de0caa9c2eefa3e3c300b9f96a025
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2025-11-03 10:50:27 -0300

    chore(pkg): replace deprecated context keys

--- pkg/sphinx/middlewares.go
+++ pkg/sphinx/middlewares.go
@@ -6,9 +6,15 @@ import (
 	"strings"
 
 	"github.com/kgjoner/cornucopia/v2/helpers/apperr"
-	"github.com/kgjoner/cornucopia/v2/helpers/controller"
 	"github.com/kgjoner/cornucopia/v2/helpers/presenter"
 	"github.com/kgjoner/sphinx/internal/common/errcode"
+)
+
+type ctxKey string
+
+const (
+	ActorCtxKey ctxKey = "sphinx_actor"
+	TokenCtxKey ctxKey = "sphinx_token"
 )
 
 type Middlewares struct {
@@ -40,8 +46,9 @@ func (m Middlewares) Authenticate(next http.Handler) http.Handler {
 		}
 
 		ctx := r.Context()
-		ctx = context.WithValue(ctx, controller.ActorKey, *user)
-		ctx = context.WithValue(ctx, controller.TokenKey, tokenStr)
+		ctx = context.WithValue(ctx, ActorCtxKey, *user)
+		ctx = context.WithValue(ctx, TokenCtxKey, tokenStr)
+		ctx = context.WithValue(ctx, presenter.ActorLogKey, user.ID)
 		r = r.WithContext(ctx)
 
 		next.ServeHTTP(w, r)
@@ -65,7 +72,7 @@ func (m Middlewares) TryAuthenticate(next http.Handler) http.Handler {
 func (m Middlewares) Guard(roles ...string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			actorValue := r.Context().Value(controller.ActorKey)
+			actorValue := r.Context().Value(ActorCtxKey)
 			if actorValue == nil {
 				err := apperr.NewUnauthorizedError("no actor found, user must be authenticated prior guard middleware", errcode.InvalidAccess)
 				presenter.HTTPError(err, w, r)
