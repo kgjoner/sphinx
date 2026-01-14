2523761577aea7f4489547bc03e89c28b055a835
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2026-01-14 16:41:43 -0300

    feat(cmd): create migrate command

---
+++ cmd/migrate/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/kgjoner/sphinx/internal/config"
+	"github.com/kgjoner/sphinx/internal/repositories/base/migrations"
+)
+
+func main() {
+	config.Must()
+	dbURL := config.Env.DATABASE_URL
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable is required")
+	}
+
+	migrationFiles := migrations.Files
+	// 1. Load migrations from embedded filesystem
+	d, err := iofs.New(migrationFiles, ".")
+	if err != nil {
+		log.Fatalf("failed to create iofs driver: %v", err)
+	}
+
+	// 2. Initialize the migrator
+	m, err := migrate.NewWithSourceInstance("iofs", d, dbURL)
+	if err != nil {
+		log.Fatalf("failed to initialize migrator: %v", err)
+	}
+
+	// 3. Execute "Up" migrations
+	fmt.Println("Running migrations...")
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("No new migrations to apply.")
+		} else {
+			log.Fatalf("failed to run migrations: %v", err)
+		}
+	} else {
+		fmt.Println("Migrations applied successfully!")
+	}
+}
