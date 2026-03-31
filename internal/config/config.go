e244a7bc22b138e7c02ab8672c4d3098bca63ccc
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2026-03-31 12:33:13 -0300

    feat(server): remove some env variable defaults

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,28 +9,28 @@ import (
 )
 
 var Env struct {
-	// If "development" is set, CORS will allow all origins and SMTP connection will be insecure (TLS disabled).
-	// Use only for local development.
-	APP_ENV         string   `envconfig:"default=production"`
-	SCHEME          string   `envconfig:"default=http"`
-	HOST            string   `envconfig:"default=localhost:8080"`
-	APP_VERSION     string   `envconfig:"default=v1.0.0"`
-	DATABASE_URL    string   `envconfig:"default=postgres://postgres:postgres@localhost:5432/sphinx?sslmode=disable"`
-	REDIS_URL       string   `envconfig:"default=redis://localhost:6379/0"`
-	ROOT_APP_ID     string   `envconfig:"default=80cadd74-5ccd-41c4-9938-3c8961be04db"`
-	// Comma-separated list of allowed origins for CORS. If not set, it will default to the CLIENT.BASE_URL. 
+	// If "development" is set, SMTP connection will be insecure (TLS disabled) and CORS will allow all origins
+	// (it may be overridden by ALLOWED_ORIGINS env variable). Use only for local development.
+	APP_ENV      string `envconfig:"default=production"`
+	SCHEME       string `envconfig:"default=http"`
+	HOST         string `envconfig:"default=localhost:8080"`
+	APP_VERSION  string `envconfig:"default=v1.0.0"`
+	DATABASE_URL string
+	REDIS_URL    string
+	ROOT_APP_ID  string `envconfig:"default=80cadd74-5ccd-41c4-9938-3c8961be04db"`
+	// Comma-separated list of allowed origins for CORS. If not set, it will default to the CLIENT.BASE_URL.
 	// In development environment, it will default to allowing all origins.
 	ALLOWED_ORIGINS []string `envconfig:"optional"`
 
-	//Set 0 for disabling concurrent sessions control
+	// Set 0 for disabling concurrent sessions control
 	MAX_CONCURRENT_SESSIONS    int `envconfig:"default=0"`
 	AUTH_GRANT_LIFETIME_IN_SEC int `envconfig:"default=300"`
 	JWT                        struct {
 		ACCESS_LIFETIME_IN_SEC  int    `envconfig:"default=900"`
 		REFRESH_LIFETIME_IN_SEC int    `envconfig:"default=172800"`
-		ALGORITHM               string `envconfig:"default=RS256"`     // RS256 or HS256 (legacy)
-		SECRET                  string `envconfig:"default=topsecret"` // Legacy HS256 secret (for initial grace period)
-		ENCRYPTION_KEY          string `envconfig:"default=changeme"`
+		ALGORITHM               string `envconfig:"default=RS256"` // RS256 or HS256 (legacy)
+		SECRET                  string // Legacy HS256 secret (for initial grace period)
+		ENCRYPTION_KEY          string
 		// How often to rotate keys (in hours), default 1 year. Set 0 to disable automatic rotation.
 		KEY_ROTATION_INTERVAL_HOURS int `envconfig:"default=8760"`
 	}
@@ -44,14 +44,14 @@ var Env struct {
 	APP_NAME          string `envconfig:"default=Sphinx"`
 	APP_STYLE_URL     string `envconfig:"optional"` // if none is provided, the default style will be used (see assets/style/style.go)
 	APP_LOGO_URL      string `envconfig:"optional"` // if none is provided, the default logo will be used (see assets/img/logo.svg)
-	SUPPORT_EMAIL     string `envconfig:"default=support@example.com"`
+	SUPPORT_EMAIL     string `envconfig:"default=support@example.test"`
 	FALLBACK_LANGUAGE string `envconfig:"default=pt"`
 
 	SMTP struct {
-		USERNAME string `envconfig:"default=http://localhost:8081/v1"`
-		PASSWORD string `envconfig:"default=topsecret"`
-		HOST     string `envconfig:"default=topsecret"`
-		PORT     string `envconfig:"default=topsecret"`
+		USERNAME string
+		PASSWORD string
+		HOST     string
+		PORT     string
 	}
 
 	// Used to protect the Swagger UI with basic authentication. The expected format is a JSON
